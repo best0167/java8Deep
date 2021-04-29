9cf27e003b2322a73445dd778814bfc0013c04aa
frame <best0167@naver.com>
2021-04-29 22:52:01 +0900

    자바 8 제대로 알기

--- src/main/java/me/best0167/java8to11/App.java
+++ src/main/java/me/best0167/java8to11/App.java
@@ -1,18 +1,67 @@
 package me.best0167.java8to11;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) {
 
-        DefaultGreeting defaultGreeting = new DefaultGreeting("nika");
+        List<String> names = new ArrayList<>();
+        names.add("nika");
+        names.add("kani");
+        names.add("eric");
+        names.add("foo");
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        names.sort(compareToIgnoreCase.reversed());
+
+        /*names.sort(String::compareToIgnoreCase);*/
+
+        names.forEach(System.out::println);
+
+        /*names.removeIf(s -> s.startsWith("k"));
+
+        names.forEach(System.out::println);
+        *//*nika
+        eric
+        foo*/
+
+        /*Set<String> k = names.stream().map(String::toUpperCase)
+                .filter(s -> s.startsWith("K")) // K 로 시작하는것만 남겨서
+                .collect(Collectors.toSet());*/ // KANI
+
+        /*long k = names.stream().map(String::toUpperCase)
+                .filter(s -> s.startsWith("K")) // K 로 시작하는것만 남겨서
+                .count();*/  // 1
+
+        /*System.out.println(k);*/
+
+
+/*        Spliterator<String> spliterator = names.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("==================");
+        while (spliterator1.tryAdvance(System.out::println));*/
+
+        /*   names.forEach(System.out::println);*/
+
+        /*name.forEach(s -> {
+                    System.out.println(s);
+                };*/
+
+    /*  for (String n: names) {
+            System.out.println(n);
+        }*/
+
+        /*DefaultGreeting defaultGreeting = new DefaultGreeting("nika");
         defaultGreeting.printName();
         defaultGreeting.printNameUpperCase();
+
+        DefaultGreeting.printAnything();*/
 
 /*        // 임의 객체의 인스턴스 메소드 참조
         String[] names = {"Nika", "kani", "eric"};
--- src/main/java/me/best0167/java8to11/DefaultGreeting.java
+++ src/main/java/me/best0167/java8to11/DefaultGreeting.java
@@ -1,6 +1,6 @@
 package me.best0167.java8to11;
 
-public class DefaultGreeting implements GreetingInterface{
+public class DefaultGreeting implements GreetingInterface, Bar {
 
     String name;
 
@@ -8,16 +8,25 @@ public class DefaultGreeting implements GreetingInterface{
         this.name = name;
     }
 
-    @Override
+/*    @Override
     public void printNameUpperCase() {
         System.out.println(this.name.toUpperCase());
-    }
+    }*/
 
     // @implSpec. 이 구현체는 getName() 으로 가져온 문자열을 대문자로 바꿔 출력한다.
     @Override
     public void printName() {
         /*System.out.println("DefaultFoo");*/
         System.out.println(this.name);
+    }
+
+    @Override
+    public void printNameUpperCase() {
+        GreetingInterface.super.printNameUpperCase();
+    }
+
+    static void printAnything() {
+        System.out.println("DefaultGreeting");
     }
 
     @Override
